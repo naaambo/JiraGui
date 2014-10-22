802b3e19a90820d75df63e3f586c2c2bef85b6c9
naaambo <sam.uk.jones@gmail.com>
2014-10-22 11:17:56 +0100

    Changed to WPF
    Update Console to GUI:
    
    -Added ButtonClick Method
    This method calls the RunASync method via a Lambda Expression. This
    expression is used as calling RunASync as a static method will cause
    errors.

---
+++ JiraGUI/Issue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JiraGUI
+{
+	public class Issue
+	{
+		[JsonProperty("expand")]
+		public string Expand { get; set; }
+		[JsonProperty("iD")]
+		public double ID { get; set; }
+		[JsonProperty("key")]
+		public string Key { get; set; }
+		[JsonProperty("self")]
+		public string Self { get; set; }
+		[JsonProperty("fields")]
+		public Fields MyFields { get; set; }
+
+		//TODO: Get access to Fields:Description
+		public class Fields
+		{
+			[JsonProperty("summary")]
+			public string Summary { get; set; }
+			[JsonProperty("description")]
+			public string Description { get; set; }
+		}
+	}
+}
---
+++ JiraGUI/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using JiraGUI;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JiraGUI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+		}
+
+		static async Task<Issue> RunASync(string inputKey)
+		{
+			var input = inputKey;
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri("https://icsasoftware.atlassian.net/rest/");
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+				client.DefaultRequestHeaders.Add("Authorization", "Basic " + BasicEncode());
+
+				try
+				{
+					HttpResponseMessage response = await client.GetAsync("api/2/issue/BPC-" + input);
+					var result = response.Content.ReadAsStringAsync().Result;
+					Issue issue = JsonConvert.DeserializeObject<Issue>(result);
+					Console.WriteLine("{0}\n\n{1}\n\n{2}", issue.Self, issue.Key, issue.MyFields.Description);
+					response.EnsureSuccessStatusCode();
+					return issue;
+				}
+				catch (HttpRequestException e)
+				{
+					Console.WriteLine("{0}", e);
+					return null;
+				}
+			}
+		}
+
+		static public string BasicEncode()
+		{
+			var bytes = Encoding.UTF8.GetBytes("sam.jones@icsasoftware.com:Jira2014");
+			var base64 = Convert.ToBase64String(bytes);
+			return base64;
+
+		}
+
+		private void ButtonClick(object sender, RoutedEventArgs e)
+		{
+			Issue result;
+
+			string inputKey = txtInput.Text;
+			RunASync(inputKey).ContinueWith(r =>
+				{
+					Console.WriteLine("first >" + r.Result);
+					result = r.Result;
+					Console.WriteLine(result);
+					this.Dispatcher.Invoke(() =>
+						{
+							txtOutput.Text = result.Key + "\n" + result.MyFields.Summary;
+						});
+
+			});
+
+
+
+		}
+	}
+}
